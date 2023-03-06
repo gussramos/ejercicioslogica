4f68207995b55b9db611033de9d00c51a5630613
gussramos <gusalzate2021@gmail.com>
2023-03-06 14:54:57 -0500

    Create capturarteclado.py

---
+++ capturarteclado.py
@@ -0,0 +1,8 @@
+name = input("your name: ")
+surname = input("your surname: ")
+email = input("your email: ")
+phone = input("your cell phone: ")
+city = input("where are you from: ")
+
+print(name, " ", surname, " email: ", email,
+      ' phone: ', phone, ' CITY OF BIRTH: ', city)
